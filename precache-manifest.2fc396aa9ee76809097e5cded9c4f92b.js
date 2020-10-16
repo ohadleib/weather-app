409b6226360e39b2b4c713f198e105d30eb6529e
אוהד ליבוביץ <ohad@whds-MacBook-Pro.local>
2020-10-16 23:57:13 +0300

    Updates

---
+++ precache-manifest.2fc396aa9ee76809097e5cded9c4f92b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cebf81657f36ee566095c1c2f4d36d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "60fd3d7da1c60c448fb4",
+    "url": "/weather-app/static/css/main.ea94b481.chunk.css"
+  },
+  {
+    "revision": "5ccdd789026fe6fc158c",
+    "url": "/weather-app/static/js/2.ee37d840.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/weather-app/static/js/2.ee37d840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fd3d7da1c60c448fb4",
+    "url": "/weather-app/static/js/main.164331ba.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
